ec1e051e883885b5f753db12ae642a05754ae642
ranks72 <ranksdead.id@gmail.com>
2021-12-27 23:56:25 +0700

    edit new notifikasi

--- src/handler.js
+++ src/handler.js
@@ -205,7 +205,7 @@ const editBookByIdHandler = (request, h) => {
   if (!name) {
     const response = h.response({
       status: 'fail',
-      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
     });
     response.code(400);
     return response;
@@ -214,7 +214,7 @@ const editBookByIdHandler = (request, h) => {
   if (readPage > pageCount) {
     const response = h.response({
       status: 'fail',
-      message: `Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount`,
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
     });
 
     response.code(400);
